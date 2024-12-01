3430c0f62504d5e6f7aed13644753582c06535bd
Francisco Altimari <francisco@altimari.nl>
2024-12-01 22:03:15 +0100

    Solve D1P2

--- 1/solution.js
+++ 1/solution.js
@@ -22,23 +22,35 @@ const sortAndSeparate = input => input
         }, 0)
     }, 0);
 
-
-
-
 console.log('1) eg: ', sortAndSeparate(eg));
 console.log('1) input: ', sortAndSeparate(input));
 
 // Part 2 ---------------------------------------------------------------------
 
-// const placeholder = () => {};
+const sortAndScore = input => input
+    .split('\n')
+    .map(i => i
+        .split('   ')
+        .map(i => Number(i))
+    )
+    .reduce(([left, right], line ) => {
+        return [[...left, line[0]], [...right, line[1]]]
+    }, [[], []])
+    .reduce((score, column, _, array) => {
+        const [left, right] = array;
 
-// console.log('2) eg: ', placeholder(eg));
-// console.log('2) input: ', placeholder(input));
+        return left.reduce((probability, number) => {
+            return probability + number * right.filter(i => i === number).length
+        }, 0)
+    }, 0);
+
+console.log('2) eg: ', sortAndScore(eg));
+console.log('2) input: ', sortAndScore(input));
 
 /*
 Wrong guesses:
 
 Correct:
     1) 765748
-    2) 
+    2) 27732508
 */
