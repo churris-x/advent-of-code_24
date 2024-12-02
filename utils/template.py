4ccd877ea9f855f6dbd4d59be8463b876129af37
Francisco Altimari <francisco@altimari.nl>
2024-12-02 01:54:05 +0100

    Add python template

---
+++ utils/template.py
@@ -0,0 +1,27 @@
+eg = open('eg.txt', 'r').read()[:-1]
+input = open('input.txt', 'r').read()[:-1]
+
+
+# Part 1 ----------------------------------------------------------------------
+
+def placeholder(input):
+    return input
+
+print('1) eg: ',    placeholder(eg))
+# print('1) input: ', placeholder(eg))
+
+# Part 2 ----------------------------------------------------------------------
+
+# def placeholder(input):
+#     return input
+
+# print('2) eg: ',    placeholder(eg))
+# print('2) input: ', placeholder(eg))
+
+'''
+Wrong guesses:
+
+Correct:
+    1) 76574
+    2) 2773250
+'''
