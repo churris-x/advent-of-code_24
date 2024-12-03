f846e663e52452a57773816b5ef4f9ff2be6d76c
Francisco Altimari <francisco@altimari.nl>
2024-12-03 13:09:09 +0100

    D3P2 progress

--- 3/solution.js
+++ 3/solution.js
@@ -5,6 +5,7 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 // Part 1 ---------------------------------------------------------------------
 
 // mul(123,123)
+// TODO: don't use regex
 
 const parse = input => input
     .split('\n')
@@ -24,15 +25,41 @@ console.log('1) input: ', parse(input));
 
 const parseInstructions = input => input
     .split('\n')
+    .flatMap(line => line.split(''))
+    .reduce(([prevInstruction, multiply, total], char) => {
+        const instruction = prevInstruction + char;
 
+        if (instruction.includes( 'don\'t()')) return ['', false, total]
+        if (instruction.includes( 'do()')) return ['', true, total]
+
+        mulIndex = instruction.indexOf('mul(')
+
+        // See if we have mul( prefix and a closing )
+        if (mulIndex >= 0 && mulIndex < instruction.indexOf(')')) {
+            if (!multiply) return ['', multiply, total]
+
+            const [a, b] = instruction
+                .slice(mulIndex + 4, -1)
+                .split(',')
+                .map(n => Number(n))
+
+            if (isNaN(a) || isNaN(b)) return ['', multiply, total]
+
+            return ['', multiply, total + a * b]
+        }
+
+
+        return [instruction, multiply, total]
+
+    }, ['', true, 0])[2]
 
 
 console.log('2) eg: ', parseInstructions(eg));
-// console.log('2) input: ', placeholder(input));
+console.log('2) input: ', parseInstructions(input));
 
 /*
 Wrong guesses:
-
+    2) 2291705 too low
 Correct:
     1) 161085926
     2) 
