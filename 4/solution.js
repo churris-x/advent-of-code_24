a760395998c5205217213632bae9fa4205a784e3
Francisco Altimari <francisco@altimari.nl>
2024-12-04 18:29:30 +0100

    D4P1 progress

--- 4/solution.js
+++ 4/solution.js
@@ -20,8 +20,15 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 
 n x n square = 2n - 1 diagonals?
 
-*/
+0123
+XMAS
 
+diagonal
+
+[0], [01, 10], [02, 11, 20], [30, 21, 12, 03],
+[40, 31, 22, 13, 04]
+
+*/
 
 
 const placeholder = input => {
@@ -35,12 +42,23 @@ const placeholder = input => {
 
     const size = normal[0].length
 
+    // let diagonal_up = []
+    // for (const line of normal) {
+    // }
 
-    // const diagonal_up =
+    const diagonal_up = normal
+        .map(line => {
+
+        })
 
 
-    console.log(normal, backwards, size);
+    // console.log(normal, backwards, size);
 }
+
+
+const parse = grid => grid
+    .reduce((total, line) => line.match(/XMAS/g).length, 0)
+
 
 console.log('1) eg: ', placeholder(eg));
 // console.log('1) input: ', placeholder(input));
