7c7cbcd6902228d35a77252281b90d33b2837d36
Francisco Altimari <francisco@altimari.nl>
2024-12-05 13:28:39 +0100

    D4P2 progress

--- 4/solution.js
+++ 4/solution.js
@@ -28,6 +28,11 @@ diagonal
 [0], [01, 10], [02, 11, 20], [30, 21, 12, 03],
 [40, 31, 22, 13, 04]
 
+
+x + y => diagonal
+x - y + n => second diagonal
+
+
 */
 
 const getAmount = input => {
@@ -76,7 +81,7 @@ const getAmount = input => {
         .reverse()
         .map(i => i.split('').reverse().join(''));
 
-    // console.table({north, south, east, west, north_east, south_west, south_east, north_west });
+    console.table({north, south, east, west, north_east, south_west, south_east, north_west });
 
     const parse = grid => grid
         .reduce((total, line) => total + (line.match(/XMAS/g)?.length ?? 0), 0);
@@ -92,15 +97,69 @@ const getAmount = input => {
 
 }
 
-console.log('1) eg: ', getAmount(eg));
-console.log('1) input: ', getAmount(input));
+// console.log('1) eg: ', getAmount(eg));
+// console.log('1) input: ', getAmount(input));
 
 // Part 2 ---------------------------------------------------------------------
 
-// const placeholder = () => {};
+/*
+ ___________
+0|    .    |
+1|   M .   |
+2|  . . .  |
+3| S # M . |
+4|. . . . .|
+5| . S A M |
+6|  . . .  |
+7|   . S   |
+8|    .    |
+ ___________
 
-// console.log('2) eg: ', placeholder(eg));
-// console.log('2) input: ', placeholder(input));
+*/
+
+const findCross = input => {
+
+    const east = input.split('\n');
+    const size = east[0].length;
+    const north_east = Array(2 * size - 1).fill('');
+    const south_east = Array(2 * size - 1).fill('');
+
+    for (const [lineIndex, line] of east.entries()) {
+        for (const [index, char] of line.split('').entries()) {
+            const row = index + lineIndex;
+            north_east[row] = north_east[row].concat(char);
+        }
+    }
+
+    for (const [lineIndex, line] of east.entries()) {
+        for (const [index, char] of line.split('').entries()) {
+            const row = index - lineIndex + size - 1;
+            south_east[row] = south_east[row].concat(char);
+        }
+    }
+
+    // south_east.reverse()
+
+
+
+    // console.table({north_east, south_east});
+    console.log(' _012345678_012345678');
+    console.log(`0|    ${north_east[0].split('').join(' ')}    |    ${south_east[0].split('').join(' ')}`, );
+    console.log(`1|   ${north_east[1].split('').join(' ')}   |   ${south_east[1].split('').join(' ')}`,    );
+    console.log(`2|  ${north_east[2].split('').join(' ')}  |  ${south_east[2].split('').join(' ')}`,       );
+    console.log(`3| ${north_east[3].split('').join(' ')} | ${south_east[3].split('').join(' ')}`,          );
+    console.log(`4|${north_east[4].split('').join(' ')}|${south_east[4].split('').join(' ')}`,             );
+    console.log(`5| ${north_east[5].split('').join(' ')} | ${south_east[5].split('').join(' ')}`,          );
+    console.log(`6|  ${north_east[6].split('').join(' ')}  |  ${south_east[6].split('').join(' ')}`,       );
+    console.log(`7|   ${north_east[7].split('').join(' ')}   |   ${south_east[7].split('').join(' ')}`,    );
+    console.log(`8|    ${north_east[8].split('').join(' ')}    |    ${south_east[8].split('').join(' ')}`, );
+
+    // for (const hu of north_east) console.log(hu, hu.length);
+
+};
+
+console.log('\n2) eg: ', findCross(eg));
+// console.log('2) input: ', findCross(input));
 
 /*
 Wrong guesses:
