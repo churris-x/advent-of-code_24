a431c1defd47638617e5e7c3dfa2ab1565660799
Francisco Altimari <francisco@altimari.nl>
2024-12-07 19:26:23 +0100

    Translate D7P2 to python

--- 7/solution.py
+++ 7/solution.py
@@ -37,16 +37,41 @@ print('1) input: ', sum_multiply(input))
 
 # Part 2 ----------------------------------------------------------------------
 
-# def placeholder(input):
-#     return input
+def concatenate(input):
+    equations = input.splitlines()
 
-# print('2) eg:    ',    placeholder(eg))
-# print('2) input: ', placeholder(input))
+    valid = 0
+
+    for equation in equations:
+
+        colon = equation.index(': ')
+
+        total = int(equation[:colon])
+        numbers = equation[colon + 2:].split(' ')
+        numbers = [int(item) for item in numbers]
+
+        def recursive_sum(array, total=None, results=[]):
+            if (not len(array)): return results.append(total)
+
+            recursive_sum(array[1:], int(f'{('', total)[bool(total)]}{array[0]}'), results)
+            recursive_sum(array[1:], (0, total)[bool(total)] + array[0], results)
+            recursive_sum(array[1:], (1, total)[bool(total)] * array[0], results)
+
+            return results
+
+        results = recursive_sum(numbers)
+
+        if total in results: valid += total
+
+    return valid
+
+print('2) eg:    ', concatenate(eg))
+print('2) input: ', concatenate(input))
 
 '''
 Wrong guesses:
 
 Correct:
-    1)
-    2)
+    1) 945512582195
+    2) 271691107779347
 '''
