c29e36de2785311a27f76b9afc293ee9cfcb0d4b
Francisco Altimari <francisco@altimari.nl>
2024-12-07 16:09:25 +0100

    Comment out prints that use the puzzle input

--- utils/template.py
+++ utils/template.py
@@ -8,15 +8,15 @@ def placeholder(input):
     return input
 
 print('1) eg: ',    placeholder(eg))
-print('1) input: ', placeholder(input))
+# print('1) input: ', placeholder(input))
 
 # Part 2 ----------------------------------------------------------------------
 
 # def placeholder(input):
 #     return input
 
-print('2) eg:    ',    placeholder(eg))
-print('2) input: ', placeholder(input))
+# print('2) eg:    ',    placeholder(eg))
+# print('2) input: ', placeholder(input))
 
 '''
 Wrong guesses:
