4778d51ed6710f78b76108cff3aa31dc827f199a
Francisco Altimari <francisco@altimari.nl>
2024-12-07 20:06:25 +0100

    D5P2 progress

--- 5/solution.js
+++ 5/solution.js
@@ -74,14 +74,53 @@ const getPages = input => {
 };
 
 console.log('1) eg:    ', getPages(eg));
-console.log('1) input: ', getPages(input));
+// console.log('1) input: ', getPages(input));
 
 // Part 2 ---------------------------------------------------------------------
+/*
 
-// const placeholder = () => {};
+97     61     53     29     13
+97|61  61|53  53|29  29|13
+97|53  61|29  53|13
+97|29  61|13
+97|13
 
-// console.log('2) eg:    ', placeholder(eg));
-// console.log('2) input: ', placeholder(input));
+*/
+
+const getCorrectPages = input => {
+    const [rulebook, batch] = input.split('\n\n');
+
+    return batch.split('\n').map(example => {
+        const pages = example.split(',')
+
+        const combinations = nChooseK(pages, 2);
+        const validRules = [];
+
+        for (const [a, b] of combinations) {
+            const rule = `${a}|${b}`;
+            const flip = `${b}|${a}`;
+
+            if (rulebook.includes(rule)) validRules.push(rule);
+            if (rulebook.includes(flip)) validRules.push(flip);
+        }
+
+        const valid = validRules.map(rule => {
+            const [a, b] = rule.split('|');
+
+            if (pages.indexOf(a) > pages.indexOf(b)) return [`${a}|${b}`, false]
+            return [`${a}|${b}`, true]
+        })
+
+
+        return valid
+
+        // return Number(pages[pages.length / 2 - .5])
+    })
+    // .reduce((sum, page) => sum + page, 0)
+};
+
+console.log('2) eg:    ', getCorrectPages(eg));
+// console.log('2) input: ', getCorrectPages(input));
 
 /*
 Wrong guesses:
