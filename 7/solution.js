d3c71051a674e0cd9ffdea243507d6f2fa50e77c
Francisco Altimari <francisco@altimari.nl>
2024-12-07 17:18:20 +0100

    Remove unused code

--- 7/solution.js
+++ 7/solution.js
@@ -12,25 +12,6 @@ NOTE => always evaluated left-to-right
 
 */
 
-const nChooseK = (array, k) => {
-    let n = array.length
-    let head, tail, result = [];
-
-    if (k > n || k < 1) return [];
-    if (k === n) return [array];
-    if (k === 1 ) return array.map(item => [item]);
-
-    for (let i = 0; i < n - k + 1; i++) {
-        head = array.slice(i, i + 1);
-        tail = nChooseK(array.slice(i + 1), k - 1);
-
-        for (let j = 0; j < tail.length; j++) {
-            result.push( head.concat(tail[ j ]));
-        }
-    }
-    return result;
-}
-
 const sumMultiply = input => input
     .split('\n')
     .map(equation => {
