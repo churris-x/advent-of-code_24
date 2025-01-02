33df4ab7341ecaae5df977b23f17f6f63d97f102
Francisco Altimari <francisco@altimari.nl>
2025-01-02 21:00:21 +0100

    Cleanup D6P1

--- 6/solution.js
+++ 6/solution.js
@@ -4,7 +4,6 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 
 // Part 1 ---------------------------------------------------------------------
 /*
-
   0123456789
 0|....#.....
 1|.........#
@@ -20,25 +19,19 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 Two arrays, one for columns and one for rows
 
 I need to preserve the x y coordinate between arrays
-
-
 */
 
 const walk = (string, index) => `${string.slice(0, index)}X${string.slice(index + 1)}`;
 
 const route = input => {
     const rows = input.split('\n');
+    const columns = rows.map((row, index) => rows.map(i => i[index]).join(''));
     const size = rows[0].length;
 
     let y = rows.findIndex(row => row.includes('^'));
     let x = rows[y].indexOf('^');
-
-    const columns = rows.map((row, index) => rows.map(i => i[index]).join(''));
-
     let direction = 'N';
-
     let inBounds = (x >= 0 && x < size) && (y >= 0 && y < size);
-
 
     const goNorth = () => {
         direction = 'N';
@@ -63,26 +56,18 @@ const route = input => {
 
     while (inBounds) {
         switch (direction) {
-            case ('N'): {
-                const next = columns[x][y - 1];
-                if (next === '#') {goEast()} else goNorth();
+            case ('N'):
+                if (columns[x][y - 1] == '#') { goEast() } else goNorth();
                 break;
-            }
-            case ('S'): {
-                const next = columns[x][y + 1];
-                if (next === '#') {goWest()} else goSouth();
+            case ('S'):
+                if (columns[x][y + 1] == '#') { goWest() } else goSouth();
                 break;
-            }
-            case ('E'): {
-                const next = rows[y][x + 1];
-                if (next === '#') {goSouth()} else goEast();
+            case ('E'):
+                if (rows[y][x + 1] == '#') { goSouth() } else goEast();
                 break;
-            }
-            case ('W'): {
-                const next = rows[y][x - 1];
-                if (next === '#') {goNorth()} else goWest();
+            case ('W'):
+                if (rows[y][x - 1] == '#') { goNorth() } else goWest();
                 break;
-            }
         }
 
         inBounds = (x >= 0 && x < size) && (y >= 0 && y < size);
